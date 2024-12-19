6f76146675a30c2da61d7c1e9ecf9f65c02c8593
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-19 20:21:09 -0300

    add: controlador de productos

---
+++ src/main/java/com/marvic/guia_modulo_6/controller/ProductController.java
@@ -0,0 +1,26 @@
+package com.marvic.guia_modulo_6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.marvic.guia_modulo_6.services.ProductService;
+
+@Controller
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductService service;
+
+    ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping()
+    public String getAllProduct(Model model) {
+        // model.addAtribute(
+        return "product_list" ;
+    }
+
+}
