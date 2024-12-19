3f0c092c501941b7f99b4224943c103e0e34c123
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-19 15:48:14 -0300

    add: controllador rest de producto

---
+++ src/main/java/com/marvic/guia_modulo_6/restControllers/ProductRestContoller.java
@@ -0,0 +1,30 @@
+package com.marvic.guia_modulo_6.restControllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.marvic.guia_modulo_6.models.Product;
+import com.marvic.guia_modulo_6.services.ProductService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/products") // Establecemos una ruta base para nuestro controller
+public class ProductRestContoller {
+
+    // Creamos una propiedad de product service
+    private final ProductService productService;
+
+    ProductRestContoller(ProductService service) {
+        // Injectamos mediante constructor el servicio a nuestro controlador
+        this.productService = service;
+    }
+
+    @GetMapping("/list")
+    public List<Product> getMethodName() {
+        return productService.getAllProducts();
+    }
+
+}
