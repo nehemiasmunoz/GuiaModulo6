5b80789fef46dd220cb0f2c2518b111b21219450
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-27 19:39:32 -0300

    add: constructor para convertir producto a dto

--- src/main/java/com/marvic/guia_modulo_6/dtos/ProductDTO.java
+++ src/main/java/com/marvic/guia_modulo_6/dtos/ProductDTO.java
@@ -1,5 +1,7 @@
 package com.marvic.guia_modulo_6.dtos;
 
+import com.marvic.guia_modulo_6.models.Product;
+
 public record ProductDTO(
         Long id,
         String name,
@@ -7,4 +9,7 @@ public record ProductDTO(
         Double price,
         Long stock) {
 
+    public ProductDTO (Product product){
+        this(product.getId(), product.getName(), product.getDescription(), product.getPrice(), product.getStock());
+    }
 }
