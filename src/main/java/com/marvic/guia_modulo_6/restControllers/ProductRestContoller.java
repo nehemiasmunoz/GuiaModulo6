1173d4f9d49bec022125838b6e3c26318000e6a4
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-30 15:20:29 -0300

    upd: endpoint de listar add: endpoints para guardar, buscar por Id y borrar

--- src/main/java/com/marvic/guia_modulo_6/restControllers/ProductRestContoller.java
+++ src/main/java/com/marvic/guia_modulo_6/restControllers/ProductRestContoller.java
@@ -1,14 +1,12 @@
 package com.marvic.guia_modulo_6.restControllers;
 
 import com.marvic.guia_modulo_6.dtos.ProductDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.marvic.guia_modulo_6.dtos.product.NewProductDTO;
 import com.marvic.guia_modulo_6.services.ProductService;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/rest/products") // Establecemos una ruta base para nuestro controller
@@ -22,9 +20,30 @@ public class ProductRestContoller {
         this.productService = service;
     }
 
-    @GetMapping("/list")
+    @GetMapping
     public List<ProductDTO> getAllProducts() {
         return productService.getAllProducts();
     }
+
+    @GetMapping("/{id}")
+    public ProductDTO getProductById(@PathVariable long id) {
+        Optional<ProductDTO> productDTO = productService.getProductById(id);
+        if (productDTO.isPresent()) {
+            return productDTO.get();
+        } else {
+            return null;
+        }
+    }
+
+    @PostMapping("/save")
+    public void saveProduct(@RequestBody NewProductDTO newProductDTO) {
+        productService.saveProduct(newProductDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable long id) {
+        productService.deleteProduct(id);
+    }
+
 
 }
