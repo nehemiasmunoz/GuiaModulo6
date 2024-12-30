f682226d3f877ea0d69cf299b4f4ef75455be81b
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-30 15:19:07 -0300

    upd: origen de datos de estatico a dinamico add:nuevos metodos dml

--- src/main/java/com/marvic/guia_modulo_6/services/ProductService.java
+++ src/main/java/com/marvic/guia_modulo_6/services/ProductService.java
@@ -1,11 +1,12 @@
 package com.marvic.guia_modulo_6.services;
 
 import com.marvic.guia_modulo_6.dtos.ProductDTO;
+import com.marvic.guia_modulo_6.dtos.product.NewProductDTO;
 import com.marvic.guia_modulo_6.models.Product;
+import com.marvic.guia_modulo_6.repository.ProductRepository;
 import com.marvic.guia_modulo_6.services.interfaces.IProductService;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -14,30 +15,35 @@ import java.util.stream.Collectors;
 // administrada por el
 @Service
 public class ProductService implements IProductService {
-    ArrayList<Product> products = new ArrayList<>();
+    private ProductRepository productRepository;
 
-    ProductService() {
-        // Agregamos productos simulando una tabla con datos
-        products.add(new Product(1L, "Zapatos", "Zapatos de cuero", 42.000, 40L));
-        products.add(new Product(2L, "Zapatos", "Zapatos de cuero", 42.000, 40L));
-        products.add(new Product(3L, "Cartera", "Cartera de cuero color negro", 20.000, 20L));
+    ProductService(ProductRepository repository) {
+        this.productRepository = repository;
     }
 
     public List<ProductDTO> getAllProducts() {
-        return products.stream()
+        return productRepository.findAll().stream()
                 .map(ProductDTO::new)
                 .collect(Collectors.toList());
     }
 
     public Optional<ProductDTO> getProductById(Long id) {
-        Optional<Product> productdb = products.stream()
-                .filter(product -> product.getId().equals(id))
-                .findFirst();
+        Optional<Product> productdb = productRepository.findById(id);
         if (productdb.isPresent()) {
             return Optional.of(new ProductDTO(productdb.get()));
         } else {
             return Optional.empty();
         }
+    }
+
+    @Override
+    public void saveProduct(NewProductDTO product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void deleteProduct(long id) {
+        productRepository.delete(id);
     }
 
 }
