7264abeb219837069ffb8d32f79cec6e472f43ce
Nehemias Muñoz <munoz_mar@outlook.com>
2024-12-30 14:07:44 -0300

    add: metodos para guardar y eliminar

--- src/main/java/com/marvic/guia_modulo_6/services/interfaces/IProductService.java
+++ src/main/java/com/marvic/guia_modulo_6/services/interfaces/IProductService.java
@@ -1,6 +1,7 @@
 package com.marvic.guia_modulo_6.services.interfaces;
 
 import com.marvic.guia_modulo_6.dtos.ProductDTO;
+import com.marvic.guia_modulo_6.dtos.product.NewProductDTO;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,4 +11,9 @@ public interface IProductService {
     List<ProductDTO> getAllProducts();
 
     Optional<ProductDTO> getProductById(Long id);
+
+    void saveProduct(NewProductDTO product);
+
+    void deleteProduct(ProductDTO product);
+
 }
